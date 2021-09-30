5bea8fecd1fbd22c063e120cbf3e48d849a105ce
Niraj Singh <nirajsinghrana987@gmail.com>
2021-09-30 23:17:03 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/img/bg1.jpg')",
+       },
+       fontFamily: {
+        stone: ['Birthstone']
+        }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
